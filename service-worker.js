2b1d868f4d7eea515400fe7e61fc053e95ddfc28
Sam-Quint <147812603+Sam-Quint@users.noreply.github.com>
2024-06-05 11:57:41 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,26 @@ self.addEventListener('install', function(event) {
         return cache.addAll([
           '/',
           '/index.html',
-          '/styles.css',
-          '/app.js',
+          '/back/Assets/style/styles.css',
+          '/back/Assets/style/styles_game.css',
+          '/back/Assets/scripts/script.js',
+          '/back/Assets/scripts/scripts.js',
+          '/public/camera/the-color-is.html',
+          '/public/camera/give-me-color.html',
+          '/public/camera/loader.html',
+          '/public/camera/result.html',
+          '/public/camera/find-the-color.html',
+          '/back/images/Logo.png',
+          '/back/images/arrow-left-solid.svg',
+          '/back/images/bolt-solid.svg',
+          '/back/images/button.png',
+          '/back/images/camera-solid.svg',
+          '/back/images/chargement.svg',
+          '/back/images/fleches.svg',
+          '/back/images/icone_192x192.png',
+          '/back/images/icone_512x512.png',
+          '/back/images/images-solid.svg',
+          '/back/images/lamp.svg',
           // Ajoutez tous les fichiers nécessaires à la mise en cache
         ]);
       })
