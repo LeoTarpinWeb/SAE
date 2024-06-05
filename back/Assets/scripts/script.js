3c18157ec89558bca27d990f84caa14357d334c0
LeoTarpinWeb <135838829+LeoTarpinWeb@users.noreply.github.com>
2024-06-05 12:17:14 +0200

    Update script.js

--- back/Assets/scripts/script.js
+++ back/Assets/scripts/script.js
@@ -2,7 +2,62 @@
 let currentStream;
 let useFrontCamera = true;
 let flashOn = false;
+document.getElementById('camera').addEventListener('click', function() {
+    document.getElementById('camera-input').click();
+});
 
+function processImage(event) {
+    const file = event.target.files[0];
+    
+    if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        
+        reader.onload = function () {
+            const image = new Image();
+            image.src = reader.result;
+
+            image.onload = function () {
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext('2d');
+
+                canvas.width = image.width;
+                canvas.height = image.height;
+
+                context.drawImage(image, 0, 0);
+
+                const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+                const data = imageData.data;
+
+                const colorCounts = {};
+                let maxColorCount = 0;
+                let dominantColor = [0, 0, 0];
+                let brightness = 0;
+
+                for (let i = 0; i < data.length; i += 4) {
+                    const r = data[i];
+                    const g = data[i + 1];
+                    const b = data[i + 2];
+                    brightness += (r + g + b) / 3;
+
+                    const rgb = [r, g, b].join(",");
+                    colorCounts[rgb] = (colorCounts[rgb] || 0) + 1;
+
+                    if (colorCounts[rgb] > maxColorCount) {
+                        maxColorCount = colorCounts[rgb];
+                        dominantColor = [r, g, b];
+                    }
+                }
+
+                brightness /= (canvas.width * canvas.height);
+
+                const hexColor = `#${dominantColor.map(x => x.toString(16).padStart(2, '0')).join('')}`;
+
+                window.location.href = `loader.html?color=${encodeURIComponent(hexColor)}&brightness=${encodeURIComponent(brightness)}`;
+            }
+        };
+    }
+}
 async function getMedia() {
     if (currentStream) {
         currentStream.getTracks().forEach(track => track.stop());
