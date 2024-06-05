66da2278f5da66a455661918436a5201c5288e0f
LeoTarpinWeb <135838829+LeoTarpinWeb@users.noreply.github.com>
2024-06-05 11:42:59 +0200

    Update script.js

--- back/Assets/scripts/script.js
+++ back/Assets/scripts/script.js
@@ -73,15 +73,3 @@ function processImage() {
     window.location.href = `loader.html?color=${encodeURIComponent(hexColor)}&brightness=${encodeURIComponent(brightness)}`;
 }
 
-window.addEventListener('load', getMedia);
-
-// Enregistrement du Service Worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-    .then(function(registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(function(error) {
-        console.log('Service Worker registration failed:', error);
-    });
-}
